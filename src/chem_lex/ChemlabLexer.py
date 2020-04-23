793dc39f02bcfd1fe17ef71d4a1dc0d506d2c18e
Isaac Rivera-Rivas <isaac.rivera6@upr.edu>
2020-04-23 12:19:21 -0400

    Added most functionality for parser tree. Needs testing and completing todos which should be done when compound class is added.

--- src/chem_lex/ChemlabLexer.py
+++ src/chem_lex/ChemlabLexer.py
@@ -12,30 +12,37 @@ class ChemLABLexer:
     t_Rbrack = r'\]'
     t_Comma = r','
     t_Semicolon = r';'
-    t_Binoper = r'!=|<=|>=|<|>|[\*\/&\|]'
+    t_Binoper = r':=|!=|<=|>=|<|>'
     t_Equal = r'='
     t_Plus = r'\+'
     t_Minus = r'\-'
+    t_Multiplication = r'\*'
+    t_Division = r'\/'
+    t_Bond = r'&'
+    t_Balance = r'\|'
+
 
     def t_Float(self, t):
         r'\d+\.\d+'
         t.value = float(t.value)
         return t
 
+
     def t_Integer(self, t):
         r'\d+'
         t.value = int(t.value)
         return t
 
-    def t_Primtok(self, t):
-        r'form|balanced\?'
-        t.type = 'Primtok'
-        return t
 
-
-    def t_Idtok(self, t):
+    def t_Idtok(self, t): # Identifies units and prefixes as well
         r'[A-Za-z\?_][A-Za-z0-9\?_]*'
-        t.type = toks.reserved.get(t.value, 'Idtok')
+        reservedTokens = dict(toks.reserved, **(toks.supported_units))
+        reservedTokens.update(toks.unit_prefix)
+        reservedTokens.update(toks.combination_unit_prefix)
+        t.type = reservedTokens.get(t.value, 'Idtok')
+        if type(t.type) is tuple:
+            t.value = t.type
+            t.type = 'UnitTok'
         return t
 
 
@@ -56,10 +63,12 @@ class ChemLABLexer:
     def t_error(self, t):
         raise TypeError("Illegal character found scanning '%s'" % t.value[0])
 
+
     # Build the lexer
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
     
+
     # Test it output
     def test(self, data):
         self.lexer.input(data)
---
+++ src/chem_parse/ParsingUtils.py
@@ -0,0 +1,57 @@
+
+
+def printDetail(detailsDict):
+    if isinstance(detailsDict, dict) and detailsDict.get("toDetail"):
+        for element in detailsDict["details"]:
+            print(element)
+        return True
+    else:
+        return False
+
+def buildDetailsDict(term, variables):
+    details = {"toDetail":True, "details":[]}
+    varType = "" 
+    if type(term) is bool: 
+        varType = "Boolean"
+    elif type(term) is int or type(term) is float:
+        varType = "Number"
+    else:
+        varType = str(type(term))
+    details['details'].append(varType + " ::= " + str(term))
+    return details
+
+
+def idLookUp(variables, id):
+    return variables.get(id)
+
+
+def manageTermOperation(firstTerm, operator, secondTerm):
+    if operator == "*":
+        return firstTerm * secondTerm
+    elif operator == "/":
+        return firstTerm / secondTerm
+    elif operator == "+":
+        return firstTerm + secondTerm
+    elif operator == "-":
+        return firstTerm - secondTerm
+    elif operator == "!=":
+        return firstTerm != secondTerm
+    elif operator == "<=":
+        return firstTerm <= secondTerm
+    elif operator == ">=":
+        return firstTerm >= secondTerm
+    elif operator == ">":
+        return firstTerm > secondTerm
+    elif operator == "<":
+        return firstTerm < secondTerm
+    elif operator == ":=":
+        return firstTerm == secondTerm
+    elif operator == "&":
+        # TODO: Need to fill this in with bond
+        raise NotImplementedError("The & operator has not been implemented yet")
+    elif operator == "|":
+        # TODO: Need to fill this in with balance
+        raise NotImplementedError("The | operator has not been implemented yet")
+    else:
+        # TODO: Need to fill this in with an appropriate error.
+        raise NotImplementedError("The token was recognized as binoper but no oper implemented yet for "+str(p[2]))

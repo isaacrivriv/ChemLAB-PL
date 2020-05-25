5e090258e5badf6c5d9d735fe4c435c2f5bd619a
marcosgonzalez6 <marcos.gonzalez6@upr.edu>
2020-05-25 03:01:18 -0400

    Fixed bond method. Changed string representation of chemical equation.

--- src/element.py
+++ src/element.py
@@ -4,9 +4,14 @@ from periodic_table import PERIODIC_TABLE
 class Element:
 
     # constructor
-    def __init__(self,a_num):
+    def __init__(self,e):
         # Stores the dictionary with the info for the specific element
-        self.dictionary = PERIODIC_TABLE[a_num]
+        try:
+            self.dictionary = PERIODIC_TABLE[e]
+        except KeyError:
+            for _, d in PERIODIC_TABLE.items():
+                if d['symbol'] == e:
+                    self.dictionary = d
         
     def __str__(self):
         # return self.element_data['symbol']
